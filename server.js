6653176b22d32f619b4811292028636a68ffe6f5
Martin <m87.wheeler@gmail.com>
2020-06-15 16:20:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use("/", express.static(path.join(__dirname, "client")));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
